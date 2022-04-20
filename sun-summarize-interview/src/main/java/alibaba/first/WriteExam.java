2628fd4976b5ce5288e263067260a4e381901158
sunxiaoyu016 <sunxiaoyu016@ke.com>
2022-04-20 16:45:50 +0800

    个人学习纪要初始化

---
+++ sun-summarize-interview/src/main/java/alibaba/first/WriteExam.java
@@ -0,0 +1,12 @@
+package alibaba.first;
+
+/**
+ * 笔试题
+ *
+ * @summary WriteExam
+ * @author: sunxiaoyu016
+ * @Copyright (c) 2022, © 贝壳找房（北京）科技有限公司
+ * @since: 2022年04月20日 16:36:00
+ */
+public class WriteExam {
+}

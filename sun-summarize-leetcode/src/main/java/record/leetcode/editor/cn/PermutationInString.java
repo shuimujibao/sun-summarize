86c5e7fe3eda27f7434f3a02adbbae46addc7830
sunxiaoyu016 <sunxiaoyu016@ke.com>
2022-05-15 20:28:47 +0800

    算法--字符串的排列没懂

---
+++ sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/PermutationInString.java
@@ -0,0 +1,96 @@
+package record.leetcode.editor.cn;
+
+//给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false 。
+//
+// 换句话说，s1 的排列之一是 s2 的 子串 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s1 = "ab" s2 = "eidbaooo"
+//输出：true
+//解释：s2 包含 s1 的排列之一 ("ba").
+//
+//
+// 示例 2：
+//
+//
+//输入：s1= "ab" s2 = "eidboaoo"
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s1.length, s2.length <= 104
+// s1 和 s2 仅包含小写字母
+//
+// Related Topics 哈希表 双指针 字符串 滑动窗口
+// 👍 671 👎 0
+
+public class PermutationInString{
+    public static void main(String[] args) {
+        Solution solution = new PermutationInString().new Solution();
+
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution {
+
+	public boolean checkInclusion(String s1, String s2) {
+		char[] pattern = s1.toCharArray();
+		char[] text = s2.toCharArray();
+
+		int pLen = s1.length();
+		int tLen = s2.length();
+
+		int[] pFreq = new int[26];
+		int[] winFreq = new int[26];
+
+		for (int i = 0; i < pLen; i++) {
+			pFreq[pattern[i] - 'a']++;
+		}
+
+		int pCount = 0;
+		for (int i = 0; i < 26; i++) {
+			if (pFreq[i] > 0){
+				pCount++;
+			}
+		}
+
+		int left = 0;
+		int right = 0;
+		// 当滑动窗口中的某个字符个数与 s1 中对应相等的时候才计数
+		int winCount = 0;
+		while (right < tLen){
+			if (pFreq[text[right] - 'a'] > 0 ) {
+				winFreq[text[right] - 'a']++;
+				if (winFreq[text[right] - 'a'] == pFreq[text[right] - 'a']){
+					winCount++;
+				}
+			}
+			right++;
+
+			while (pCount == winCount){
+				if (right - left == pLen){
+					return true;
+				}
+				if (pFreq[text[left] - 'a'] > 0 ) {
+					winFreq[text[left] - 'a']--;
+					if (winFreq[text[left] - 'a'] < pFreq[text[left] - 'a']){
+						winCount--;
+					}
+				}
+				left++;
+			}
+		}
+		return false;
+	}
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

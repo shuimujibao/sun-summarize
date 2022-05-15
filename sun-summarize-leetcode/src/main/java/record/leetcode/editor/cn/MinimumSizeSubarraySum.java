4034617b52300ac5c3e25f69855ad8c69badb8b8
sunxiaoyu016 <sunxiaoyu016@ke.com>
2022-05-15 21:42:04 +0800

    算法--长度最小子数组

--- sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/MinimumSizeSubarraySum.java
+++ sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/MinimumSizeSubarraySum.java
@@ -56,6 +56,18 @@ public class MinimumSizeSubarraySum {
 	}
 
 	//leetcode submit region begin(Prohibit modification and deletion)
+//	阅读完题目后最容易想到的就是暴力解法，直接用两个for循环把所有情况都枚举一遍，
+//	保存满足情况的最小长度就行。但是仔细思考后发现暴力枚举会造成很多不必要的重复工作，
+//	比如说当nums[i]到nums[j]的和已经大于或等与target时就没有必要枚举nums[i]到nums[j + n]的和了；
+//	或者当nums[i]到nums[j]的和已经和已经小于target时，也就没有必要枚举nums[i + n]到nums[j]（i + n < j）的和了。
+
+//	因此，为了避免这些重复工作，我们可以维持一个滑动窗口（nums[left]到nums[right]），
+//	定义两个指针left和right分别表示子数组（滑动窗口窗口）的开始位置和结束位置，
+//	维护变量sum存储子数组中的元素和（即从nums[left]到nums[right]的元素和）。初始状态下，left和right都指向下标0，
+//	当sum<target时，将nums[right]的值加入sum中，并将指针right向右移动；
+//	当sum>=target时，更新子数组的最小长度（子数组的长度为right - left + 1），并减去nums[left]的值，将left的值向右移动，直到sum<target，具体代码如下。
+
+
 	class Solution {
 		public int minSubArrayLen(int target, int[] nums) {
 			int sum = 0;

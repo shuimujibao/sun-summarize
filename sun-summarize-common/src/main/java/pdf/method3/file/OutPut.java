5a2c1a73810aa4570cb37859e75be210c9f1a9b2
xy.sun06 <xy.sun06@zuche.com>
2022-07-30 14:56:59 +0800

    生成PDF

---
+++ sun-summarize-common/src/main/java/pdf/method3/file/OutPut.java
@@ -0,0 +1,17 @@
+package pdf.method3.file;
+
+/**
+ * OutPut
+ *
+ * @summary OutPut
+ * @author: sunxiaoyu016
+ * @Copyright (c) 2022, © 神州租车（北京）科技有限公司
+ * @since: 2022年07月30日 14:47:00
+ */
+public class OutPut implements File {
+
+	@Override
+	public void action(String path) {
+
+	}
+}

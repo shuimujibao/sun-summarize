a5fd6671a3ed08806434caca75f3622c350a57b4
xy.sun06 <xy.sun06@zuche.com>
2024-02-22 11:28:59 +0800

    feat：打家劫舍

--- sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/HouseRobber.java
+++ sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/HouseRobber.java
@@ -44,7 +44,20 @@ public class HouseRobber {
 	//leetcode submit region begin(Prohibit modification and deletion)
 	class Solution {
 		public int rob(int[] nums) {
-			return 1;
+			if (nums == null || nums.length == 0) {
+				return 0;
+			}
+			int length = nums.length;
+			if (length == 1) {
+				return nums[0];
+			}
+			int[] dp = new int[length];
+			dp[0] = nums[0];
+			dp[1] = Math.max(nums[0], nums[1]);
+			for (int i = 2; i < length; i++) {
+				dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+			}
+			return dp[length - 1];
 		}
 	}
 //leetcode suit region end(Prohibit modification and deletion)

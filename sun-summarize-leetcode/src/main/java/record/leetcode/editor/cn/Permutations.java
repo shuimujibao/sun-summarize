789bda6b430e60df7f55ff6a6df48ae5dcbdda0e
xy.sun06 <xy.sun06@zuche.com>
2024-02-29 14:08:19 +0800

    feat：全排列

--- sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/Permutations.java
+++ sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/Permutations.java
@@ -52,39 +52,45 @@ public class Permutations {
 	//leetcode submit region begin(Prohibit modification and deletion)
 	class Solution {
 
+		/**
+		 * 结果集
+		 */
 		List<List<Integer>> res = new LinkedList<>();
+
+		/**
+		 * 记录「路径」
+		 */
+		LinkedList<Integer> track = new LinkedList<>();
 
 		public List<List<Integer>> permute(int[] nums) {
 
-			// 记录「路径」
-			LinkedList<Integer> track = new LinkedList<>();
 
 			backtrack(nums, track);
 
 			return res;
 		}
 
-		// 路径：记录在 track 中
-		// 选择列表：nums 中不存在于 track 的那些元素
-		// 结束条件：nums 中的元素全都在 track 中出现
 		void backtrack(int[] nums, LinkedList<Integer> track) {
-			// 触发结束条件
+
+			// 中止条件
 			if (track.size() == nums.length) {
-				res.add(new LinkedList(track));
+				res.add(new LinkedList<>(track));
 				return;
 			}
 
-			for (int i = 0; i < nums.length; i++) {
-				// 排除不合法的选择
-				if (track.contains(nums[i]))
+			// 回溯
+			for (int num : nums) {
+				// 剪枝
+				if (track.contains(num)) {
 					continue;
-				// 做选择
-				track.add(nums[i]);
-				// 进入下一层决策树
+				}
+
+				// 路径选择
+				track.add(num);
 				backtrack(nums, track);
-				// 取消选择
 				track.removeLast();
 			}
+
 		}
 	}
 //leetcode submit region end(Prohibit modification and deletion)

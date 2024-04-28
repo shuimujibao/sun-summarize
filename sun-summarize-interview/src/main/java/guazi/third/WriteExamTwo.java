a5b2b205edf868c5d075686d2d8eab34111881ab
xy.sun0 <xy.sun06@zuche.com>
2024-04-28 18:18:09 +0800

    瓜子

---
+++ sun-summarize-interview/src/main/java/guazi/third/WriteExamTwo.java
@@ -0,0 +1,14 @@
+package guazi.third;
+
+/**
+ * @description: 两个线程交替打印AB
+ * @author: xy.sun06
+ * @create: 2024-04-26 16:08
+ * @Copyright (c) 2024, © 神州租车（北京）科技有限公司
+ */
+public class WriteExamTwo {
+
+	Object object;
+
+
+}

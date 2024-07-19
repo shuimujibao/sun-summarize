fadb800ea798308894f6cb2f7a5d0efcdeb6d7d4
xy.sun0 <xy.sun06@zuche.com>
2024-07-19 14:53:25 +0800

    fix:leetcode-前缀和

--- sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/SubarraySumEqualsK.java
+++ sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/SubarraySumEqualsK.java
@@ -37,12 +37,14 @@ package record.leetcode.editor.cn;
 //
 //
 
+import java.util.HashMap;
+
 public class SubarraySumEqualsK {
 	public static void main(String[] args) {
 		Solution solution = new SubarraySumEqualsK().new Solution();
 		int[] nums = new int[]{1, 2, 1, 2, 1};
 		int k = 3;
-		solution.subarraySum(nums, k);
+		solution.subarraySum2(nums, k);
 	}
 
 	//leetcode submit region begin(Prohibit modification and deletion)
@@ -71,6 +73,37 @@ public class SubarraySumEqualsK {
 					if (sum == k) {
 						res++;
 					}
+				}
+			}
+			return res;
+		}
+
+
+		public int subarraySum2(int[] nums, int k) {
+			int n = nums.length;
+			// 前缀和数组
+			int[] preSum = new int[n + 1];
+			preSum[0] = 0;
+			// 前缀和到该前缀和出现次数的映射，方便快速查找所需的前缀和
+			HashMap<Integer, Integer> count = new HashMap<>();
+			count.put(0, 1);
+			// 记录和为 k 的子数组个数
+			int res = 0;
+
+			// 计算 nums 的前缀和
+			for (int i = 1; i <= n; i++) {
+				preSum[i] = preSum[i - 1] + nums[i - 1];
+				// 如果之前存在值为 need 的前缀和
+				// 说明存在以 nums[i-1] 结尾的子数组的和为 k
+				int need = preSum[i] - k;
+				if (count.containsKey(need)) {
+					res += count.get(need);
+				}
+				// 将当前前缀和存入哈希表
+				if (!count.containsKey(preSum[i])) {
+					count.put(preSum[i], 1);
+				} else {
+					count.put(preSum[i], count.get(preSum[i]) + 1);
 				}
 			}
 			return res;

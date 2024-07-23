405530878d07b38337444e4facf3f2bb8c37aa7a
xy.sun0 <xy.sun06@zuche.com>
2024-07-23 16:37:24 +0800

    fix:leetcode-环形链表二

--- sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/LinkedListCycleIi.java
+++ sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/LinkedListCycleIi.java
@@ -81,19 +81,30 @@ public class LinkedListCycleIi {
 	 */
 	public class Solution {
 		public ListNode detectCycle(ListNode head) {
-			ListNode fast = head, slow = head;
-			while (true) {
-				if (fast == null || fast.next == null) return null;
+			ListNode fast, slow;
+			fast = slow = head;
+
+			while (fast != null && fast.next != null) {
 				fast = fast.next.next;
 				slow = slow.next;
 				if (fast == slow) break;
 			}
-			fast = head;
-			while (slow != fast) {
-				slow = slow.next;
-				fast = fast.next;
+
+			// 上面的代码类似 hasCycle 函数
+			if (fast == null || fast.next == null) {
+				// fast 遇到空指针说明没有环
+				return null;
 			}
-			return fast;
+
+			// 重新指向头结点
+			slow = head;
+
+			// 快慢指针同步前进，相交点就是环起点
+			while (slow != fast) {
+				fast = fast.next;
+				slow = slow.next;
+			}
+			return slow;
 		}
 	}
 }

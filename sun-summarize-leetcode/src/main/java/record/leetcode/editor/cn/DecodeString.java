cb3b9c7ca3ebe43bafd5340c43b9cfc534e240f4
xy.sun0 <xy.sun06@zuche.com>
2024-08-23 10:00:47 +0800

    fix：字符串解码

--- sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/DecodeString.java
+++ sun-summarize-leetcode/src/main/java/record/leetcode/editor/cn/DecodeString.java
@@ -62,7 +62,8 @@ import java.util.Stack;
 public class DecodeString{
     public static void main(String[] args) {
         Solution solution = new DecodeString().new Solution();
-
+	    String s = solution.decodeString2("3[a2[c]]");
+	    System.out.println(s);
     }
 
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -182,23 +183,33 @@ class Solution {
 
 
 	/**
-	 * 创建数字栈，在遍历编码字符串过程中记录出现的数字
-	 * Deque<Integer> numStack = new LinkedList<>();
-	 * <p>
-	 * 创建字符栈，在遍历编码字符串过程中记录出现的字符串
-	 * Deque<StringBuilder> strStack = new LinkedList<>();
+	 * 字符串解码
+	 *
+	 *
 	 * <p>
 	 * 输入字符串 3[a2[c]]
 	 * <p>
+	 * 思路：从里向外处理
+	 * 3[a2[c]]--->3[acc]-->accaccacc
+	 * <p>
+	 * 参数定义：
+	 * ===========numStack在遍历编码字符串过程中记录出现的数字 =================
+	 * ===========strStack在遍历编码字符串过程中记录出现的字符串 =================
+	 *
+	 * ===========digit为一次遍历出现的数字=================
+	 * ===========res为一次遍历出现的字符=================
+	 *
+	 * ==========='[' && ']' 不入栈，其作用为字符串处理节点=================
+	 * <p>
+	 *     3[a2[c]]解码过程
 	 * 遍历到3  计算出 digit = 3
-	 * 遍历到[  numStack栈中,此时为digit = 3，strStack栈中res此时为空，重置digit && res
+	 * 遍历到[  《入栈标志》此时numStack = 3，strStack =""；重置 res && digit
 	 * 遍历到a  res新增a
 	 * 遍历到2  计算出 digit = 2
-	 * 遍历到[  numStack栈中,此时为3->2,strStack栈中为a,重置digit && res
+	 * 遍历到[  《入栈标志》 此时numStack = 3->2 strStack=""->a;重置 res && digit
 	 * 遍历到c  res新增c
-	 * 遍历到]  numStack出栈2，
-	 *
-	 *
+	 * 遍历到]  《出栈标志》 numStack中2出栈,strStack中a出栈,循环组装 res = acc;numStack = 3 strStack="";
+	 * 遍历到]  《出栈标志》  numStack中3出栈, strStack中""出栈,循环组装 res = accaccacc; 遍历结束输出res
 	 *
 	 *
 	 * <a href="https://blog.algomooc.com/LeetCode/%E7%AC%AC%E4%B8%80%E5%91%A8/%E7%AC%AC%E5%85%AD%E5%A4%A9/LeetCode%20394%E3%80%81%E5%AD%97%E7%AC%A6%E4%B8%B2%E8%A7%A3%E7%A0%81.html#%E4%B8%80%E3%80%81%E9%A2%98%E7%9B%AE%E6%8F%8F%E8%BF%B0">...</a>
@@ -209,93 +220,34 @@ class Solution {
 	public String decodeString2(String s) {
 		// 创建数字栈，在遍历编码字符串过程中记录出现的数字
 		Deque<Integer> numStack = new LinkedList<>();
-
 		// 创建字符栈，在遍历编码字符串过程中记录出现的字符串
 		Deque<StringBuilder> strStack = new LinkedList<>();
 
-		// 在访问编码字符串的过程中，用来记录访问到字符串之前出现的数字，一开始为 0
 		int digit = 0;
-
-		// 在访问编码字符串的过程中，把得到的结果存放到 res 中
 		StringBuilder res = new StringBuilder();
 
 		// 从头到尾遍历编码字符串
 		for (int i = 0; i < s.length(); i++) {
-
-			// 在遍历过程中，字符会出现 4 种情况
-
-			// 先获取此时访问的字符
 			char ch = s.charAt(i);
-
-			// 1、如果是数字，需要把字符转成整型数字
-			// 注意数字不一定是 1 位，有可能是多位
-			// 比如 123a，在字母 a 的前面出现了 123，表示 a 重复出现 123 次
-			// 那么一开始 ch 为 1，当访问到 ch 为 2 的时候，1 和 2 要组成数字 12
-			// 再出现 3 ，12 和 3 组成数字 123
 			if (Character.isDigit(ch)) {
-
-				// 先将字符转成整型数字 ch-‘0’
-				// 补充知识：将字符'0'-'9'转换为数字，只需将字符变量减去'0'就行
-				// 因为字符和数字在内存里都是以 ASCII 码形式存储的
-				// 减去 '0' ，其实就是减去字符 '0' 的 ASCII 码，而字符 '0' 的 ASCII 码是 30
-				// 所以减去'0'也就是减去30，然后就可以得到字符对应的数字了。
 				int num = ch - '0';
-
-				// 再将这个数字和前面存储的数字进行组合
-				// 1 和 2 组成数字 12，也就是 1 * 10 + 2 = 12
-				// 12 和 3 组成数字 123，也就是 12 * 10 + 3 = 123
 				digit = digit * 10 + num;
-
-				// 2、如果是字符
 			} else if ((ch >= 'a' && ch <= 'z')) {
-
-				// 说明它就出现一次，可以直接存放到 res 中
 				res.append(ch);
-
-				// 3、如果是"["
-				// 出现了嵌套的内层编码字符串，而外层的解码需要等待内层解码的结果
-				// 那么之前已经扫描的字符需要存放起来，等到内层解码之后再重新使用
 			} else if (ch == '[') {
-
-				// 把数字存放到数字栈
 				numStack.push(digit);
-
-				// 把外层的解码字符串存放到字符串栈
 				strStack.push(res);
-
-				// 开始新的一轮解码
-				// 于是，digit 归零
 				digit = 0;
-
-				// res 重新初始化
 				res = new StringBuilder();
-
-				// 4、如果是"]"
 			} else if (ch == ']') {
-
-				// 此时，内层解码已经有结果，需要把它和前面的字符串进行拼接
-
-				// 第一步，先去查看内层解码的字符串需要被重复输出几次
-				// 比如 e3[abc]，比如内层解码结果 abc 需要输出 3 次
-				// 通过数字栈提取出次数
 				int count = numStack.poll();
-
-				// 第二步，通过字符串栈提取出之前的解码字符串
 				StringBuilder outString = strStack.poll();
-
-				// 第三步，不断的把内层解码的字符串拼接起来
 				for (int j = 0; j < count; j++) {
-
-					// 拼接到 outString 后面，拼接 count 次
 					outString.append(res.toString());
 				}
-
-				// 再把此时得到的结果赋值给 res
 				res = outString;
 			}
 		}
-
-		// 返回解码成功的字符串
 		return res.toString();
 	}
 }
